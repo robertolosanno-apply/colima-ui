18bd5db75eface289a9e0260e181666be60704a9
Roberto Losanno <roberto.losanno@applydigital.com>
2026-02-16 10:22:42 +0000

    Initial Commit

---
+++ ColimaUI/ColimaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColimaUIApp.swift
+//  ColimaUI
+//
+//  Created by Roberto Losanno on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ColimaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColimaUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColimaUI
+//
+//  Created by Roberto Losanno on 16/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
